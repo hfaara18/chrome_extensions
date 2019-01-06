feeae3e21b3ed36d06de828c888d0f0808bc787c
hfaara18 <faarahussein@gmail.com>
2019-01-06 03:48:46 -0800

    Implemented notification and context menu functionalities

--- budget_manager_extension/options.js
+++ budget_manager_extension/options.js
@@ -9,6 +9,14 @@ $(function() {
     }
   });
   $("#resetTotal").click(function() {
-    chrome.storage.sync.set({"total": 0});
+    chrome.storage.sync.set({"total": 0}, function() {
+      var resetOptions = {
+        type: "basic",
+        iconUrl: "icon48.png",
+        title: "Total Has Been Reset",
+        message: "You have reset your total spending!"
+      };
+      chrome.notifications.create("restNotif",resetOptions);
+    });
   })
 })
--- budget_manager_extension/popup.js
+++ budget_manager_extension/popup.js
@@ -5,7 +5,7 @@ $(function() {
   });
 
   $("#spendAmount").click(function() {
-    chrome.storage.sync.get("total", function(budget) {
+    chrome.storage.sync.get(["total", "limit"], function(budget) {
       var newTotal = 0;
 
       if (budget.total) {
@@ -18,7 +18,17 @@ $(function() {
         newTotal += parseInt(amount);
       }
 
-      chrome.storage.sync.set({"total": newTotal});
+      chrome.storage.sync.set({"total": newTotal}, function() {
+        if (amount && newTotal >= budget.limit) {
+          var notifOptions = {
+            type: "basic",
+            iconUrl: "icon48.png",
+            title: "Budget Limit Reached",
+            message: "You have reached your budget limit!"
+          };
+          chrome.notifications.create("limitNotif",notifOptions);
+        }
+      });
 
       $("#total").text(newTotal);
       $("#amount").val("");
