b2f84ff2a8d46e9006cec150336db0bbed0797a2
hfaara18 <faarahussein@gmail.com>
2019-01-06 04:17:59 -0800

    Implemented external budget adding fucntionality to extension

--- budget_manager_extension/eventPage.js
+++ budget_manager_extension/eventPage.js
@@ -1,7 +1,39 @@
+var contexts = ["selection"];
 var contextMenuItem = {
-  "id": "spendMoney",
   "title": "SpendMoney",
-  "contexts": ["selection"]
+  "contexts": contexts,
+  "id": "spendMoney"
 }
 
 chrome.contextMenus.create(contextMenuItem);
+
+function isInt(val) {
+  return !isNaN(val) && parseInt(Number(val)) == val &&
+  !isNaN(parseInt(val, 10));
+}
+chrome.contextMenus.onClicked.addListener(function(clickData) {
+  if(clickData.menuItemId == "spendMoney" && clickData.selectionText) {
+    if(isInt(clickData.selectionText)) {
+      chrome.storage.sync.get(["total", "limit"], function(budget) {
+        var newTotal = 0;
+
+        if (budget.total) {
+          newTotal += parseInt(budget.total);
+        }
+        newTotal += parseInt(clickData.selectionText);
+        chrome.storage.sync.set({"total": newTotal}, function() {
+          if (newTotal >= budget.limit) {
+            var notifOptions = {
+              type: "basic",
+              iconUrl: "icon48.png",
+              title: "Budget Limit Reached",
+              message: "You have reached your budget limit!"
+            };
+            chrome.notifications.create("limitNotif",notifOptions);
+            console.log("fdgldfgd flsdnfjlld");
+          }
+        })
+      })
+    }
+  }
+})
