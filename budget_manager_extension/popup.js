ddd7b2777d3ce1ec203be7ed126dd13fce493703
hfaara18 <faarahussein@gmail.com>
2019-01-06 03:27:14 -0800

    Added the options and fixed the chrome storage api bug

--- budget_manager_extension/popup.js
+++ budget_manager_extension/popup.js
@@ -1,7 +1,12 @@
 $(function() {
+  chrome.storage.sync.get(["total", "limit"], function(budget) {
+    $("#total").text(budget.total);
+    $("#limit").text(budget.limit);
+  });
+
   $("#spendAmount").click(function() {
-    chrome.storage.sync.get(["total"], function(budget) {
-      newTotal = 0;
+    chrome.storage.sync.get("total", function(budget) {
+      var newTotal = 0;
 
       if (budget.total) {
         newTotal += parseInt(budget.total);
@@ -17,6 +22,7 @@ $(function() {
 
       $("#total").text(newTotal);
       $("#amount").val("");
+
       console.log(newTotal);
     });
   });
